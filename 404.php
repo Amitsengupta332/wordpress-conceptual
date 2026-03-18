51daae6c7808495bcd975e842f84eb0fb150bf81
amitsengupta <amitsengupta332@gmail.com>
2026-03-18 13:54:26 +0600

    404.php done

---
+++ 404.php
@@ -0,0 +1,11 @@
+<section id="main-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h5>error code: 404</h5>
+                <h1>OOOPS!</h1>
+                <h4>This is not the page your are looking for!</h4>
+            </div>
+        </div>
+    </div>
+</section>
