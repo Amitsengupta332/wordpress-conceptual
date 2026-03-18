557d3e49348e9840d449c1c66df685cd1fd4a690
amitsengupta <amitsengupta332@gmail.com>
2026-03-18 13:21:27 +0600

    page are load done

--- functions.php
+++ functions.php
@@ -1,66 +1,90 @@
 <?php
 
-add_theme_support("title-tag");
-add_theme_support("post-thumbnails");
+function amit_theme_setup() {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
 
-function amit_css_js_enqueue()
-{
-    wp_enqueue_style("amit-style", get_stylesheet_uri());
-    wp_enqueue_style("bootstrap", get_template_directory_uri() . "/css/bootstrap.min.css", [], "5.3.8", "all");
-    wp_enqueue_style("custom", get_template_directory_uri() . "/css/custom.css", [], "1.0.0", "all");
-
-    wp_enqueue_script("bootstrap-js", get_template_directory_uri() . "/js/bootstrap.min.js", [], "5.3.8", true);
+    register_nav_menus(array(
+        'main-menu' => __('Main Menu', 'learning-wp'),
+    ));
 }
-add_action("wp_enqueue_scripts", "amit_css_js_enqueue");
+add_action('after_setup_theme', 'amit_theme_setup');
 
 
-function amit_customize_register($wp_customize){
-  // Header Area Customization
-  $wp_customize->add_section("amit_header_area", [
-    "title" => __("Header Area", "learning-wp"),
-  ]);
+function amit_css_js_enqueue() {
+    wp_enqueue_style('amit-style', get_stylesheet_uri());
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '5.3.8', 'all');
+    wp_enqueue_style('custom', get_template_directory_uri() . '/css/custom.css', array(), '1.0.0', 'all');
 
-  $wp_customize->add_setting("amit_logo", [
-    "default" => get_template_directory_uri() . "/images/logo.png",
-    "sanitize_callback" => "esc_url_raw",
-  ]);
-
-  $wp_customize->add_control(
-    new WP_Customize_Image_Control($wp_customize, "amit_logo", [
-      "label" => "Upload Logo",
-      "description" => "If you want you can upload or update your logo",
-      "setting" => "amit_logo",
-      "section" => "amit_header_area",
-    ])
-  );
-
-  $wp_customize->add_setting("amit_top_header", [
-    "default" => "Learning WordPress is Fun 🤩",
-    "sanitize_callback" => "sanitize_text_field",
-  ]);
-
-  $wp_customize->add_control("amit_top_header", [
-    "label" => "Top Header Text",
-    "description" => "If you want you can update your top header text.",
-    "setting" => "amit_top_header",
-    "section" => "amit_header_area",
-  ]);
-
-  // Footer Area Customization
-  $wp_customize->add_section("amit_footer_area", [
-    "title" => __("Footer Area", "learning-wp"),
-  ]);
-
-  $wp_customize->add_setting("amit_copyright_text", [
-    "default" => "&copy; Allright reserved by Amit Sengupta",
-    "sanitize_callback" => "sanitize_text_field",
-  ]);
-
-  $wp_customize->add_control("amit_copyright_text", [
-    "label" => "Copyright Text",
-    "description" => "If you want you can update your copyright text.",
-    "setting" => "amit_copyright_text",
-    "section" => "amit_footer_area",
-  ]);
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '5.3.8', true);
 }
-add_action("customize_register", "amit_customize_register");
+add_action('wp_enqueue_scripts', 'amit_css_js_enqueue');
+
+
+function amit_customize_register($wp_customize) {
+    // Header Area Customization
+    $wp_customize->add_section('amit_header_area', array(
+        'title' => __('Header Area', 'learning-wp'),
+    ));
+
+    $wp_customize->add_setting('amit_logo', array(
+        'default' => get_template_directory_uri() . '/images/logo.png',
+        'sanitize_callback' => 'esc_url_raw',
+    ));
+
+    $wp_customize->add_control(
+        new WP_Customize_Image_Control($wp_customize, 'amit_logo', array(
+            'label' => 'Upload Logo',
+            'description' => 'If you want you can upload or update your logo',
+            'settings' => 'amit_logo',
+            'section' => 'amit_header_area',
+        ))
+    );
+
+    $wp_customize->add_setting('amit_top_header', array(
+        'default' => 'Learning WordPress is Fun 🤩',
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control('amit_top_header', array(
+        'label' => 'Top Header Text',
+        'description' => 'If you want you can update your top header text.',
+        'settings' => 'amit_top_header',
+        'section' => 'amit_header_area',
+        'type' => 'text',
+    ));
+
+    // Footer Area Customization
+    $wp_customize->add_section('amit_footer_area', array(
+        'title' => __('Footer Area', 'learning-wp'),
+    ));
+
+    $wp_customize->add_setting('amit_copyright_text', array(
+        'default' => '&copy; Allright reserved by Amit Sengupta',
+        'sanitize_callback' => 'wp_kses_post',
+    ));
+
+    $wp_customize->add_control('amit_copyright_text', array(
+        'label' => 'Copyright Text',
+        'description' => 'If you want you can update your copyright text.',
+        'settings' => 'amit_copyright_text',
+        'section' => 'amit_footer_area',
+        'type' => 'textarea',
+    ));
+}
+add_action('customize_register', 'amit_customize_register');
+
+
+// Register Widget
+function amit_widgets_register() {
+    register_sidebar(array(
+        'name'          => __('Main Widget', 'learning-wp'),
+        'id'            => 'sidebar',
+        'description'   => 'Your main Sidebar',
+        'before_widget' => '<div class="widget">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3 class="widget-title">',
+        'after_title'   => '</h3>',
+    ));
+}
+add_action('widgets_init', 'amit_widgets_register');
--- index.php
+++ index.php
@@ -19,7 +19,9 @@
             <div class="container">
                 <div class="row">
                     <div class="col-m-12">
-                        <p>Learning WordPress is fun</p>
+                        <!-- <p>Learning WordPress is fun</p> -->
+                        <p><?php echo esc_html(get_theme_mod('amit_top_header', 'Learning WordPress is Fun 🤩')); ?>
+                        </p>
                     </div>
                 </div>
             </div>
@@ -37,19 +39,69 @@
                         </h1>
                     </div>
                     <div class="col-md-9">
-                        <ul id="nav">
+                        <!-- <ul id="nav">
                             <li><a href="">Home</a></li>
                             <li><a href="">Blog</a></li>
                             <li><a href="">About</a></li>
                             <li><a href="">Services</a></li>
                             <li><a href="">Contact</a></li>
-                        </ul>
+                        </ul> -->
+                        <?php
+                        wp_nav_menu(array(
+                            'theme_location' => 'main-menu',
+                            'menu_id' => 'nav',
+                        ));
+                        ?>
                     </div>
                 </div>
             </div>
         </div>
     </header>
-    <h1>Hello WordPress. This is Wp Conceptual</h1>
+
+    <!-- body part -->
+    <!-- <h1>Hello WordPress. This is Wp Conceptual</h1> -->
+
+    <section id="main-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1><?php the_title(); ?></h1>
+                      <?php the_content(); ?>
+                    <!-- <h1>Hello WordPress. This is Wp Conceptual</h1> -->
+
+                    <!-- <div class="blog_post">
+                        <?php if (have_posts()):
+                            while (have_posts()):
+                                the_post(); ?>
+                                <div class="child_post">
+                                    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                                    <?php the_post_thumbnail('post-thumbnails'); ?>
+                                    <?php the_content(); ?>
+                                </div>
+
+                            <?php endwhile;
+                        else:
+                            _e('No post found');
+                        endif; ?>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer -->
+
+    <footer id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p><?php echo wp_kses_post(get_theme_mod('amit_copyright_text', 'Copyright &copy; 2026')); ?></p>
+
+                    <!-- <p>All Rights Reserved</p> -->
+                </div>
+            </div>
+        </div>
+    </footer>
 
     <?php wp_footer() ?>
 </body>
